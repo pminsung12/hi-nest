5f904c072445fab22ed53a0d80c3720323c2f9dd
Minseong Park <pminsung12@gmail.com>
2023-10-28 00:29:33 +0900

    [feat] Movie Controller 추가하고 CRUD 컨트롤러 구현

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
