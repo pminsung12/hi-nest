f19e5d07b06ee5aec7ed59d6b279b07bcc720e80
Minseong Park <pminsung12@gmail.com>
2023-10-28 00:39:28 +0900

    [feat] GET: search+queryParameter 컨트롤러 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('movies')
@@ -13,6 +14,11 @@ export class MoviesController {
   @Get()
   getAll() {
     return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
   }
 
   @Get('/:id')
