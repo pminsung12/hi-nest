d57b87f3858397c7b318eb40abc2151366ee5ef9
Minseong Park <pminsung12@gmail.com>
2023-10-31 03:24:42 +0900

    feat: e2e-test post 400, patch 200, delete 200 케이스 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -43,7 +43,7 @@ describe('AppController (e2e)', () => {
           },
         ]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -52,6 +52,17 @@ describe('AppController (e2e)', () => {
           genres: ['test'],
         })
         .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
     });
 
     it('DELETE', () => {
@@ -66,7 +77,14 @@ describe('AppController (e2e)', () => {
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'updated test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
